5038736c36cbe6875eeb1556b32a16aae3abbabc
SiMista <simeondeiva@gmail.com>
2024-11-15 17:30:38 +0100

    New configuration done with Typescript

---
+++ backend/src/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
