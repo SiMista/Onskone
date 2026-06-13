a1c3d5727468488bc74c9d2a834fcbf956f860f2
SiMista <simeondeiva@gmail.com>
2026-06-13 11:34:19 +0200

    messsage share invitation

--- frontend/src/i18n/en.tsx
+++ frontend/src/i18n/en.tsx
@@ -71,7 +71,7 @@ export const en: Dictionary = {
     shareInvite: {
       button: 'Share the invite',
       title: 'Onskoné',
-      message: 'Hey, come play with me on Onskoné! 🎉',
+      message: "Let's see if we really know each other... join my game on Onskoné! 🎭",
     },
     minPlayers: (n) => `You need at least ${n} players to start`,
     selectAtLeastOneTheme: 'Pick at least 1 theme',
